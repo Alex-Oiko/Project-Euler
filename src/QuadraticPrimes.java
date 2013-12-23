4d1b7d9f58c4ce9a735331c06ef69bc2beaa19a0
Alex <alex.pana.oikonomou@gmail.com>
2013-12-23 17:59:46 +0200

    solved Quadratic Primes proble

--- src/QuadraticPrimes.java
+++ src/QuadraticPrimes.java
@@ -4,39 +4,38 @@ public class QuadraticPrimes {
 	public static void main(String[] args){
 		int maxNumOfPrimes=0;
 		int coefProd=1;
+		long theA=0;long theB=0;
 		
-		for(int a=0;a<1000;a++){
-			for(int b=0;b<1000;b++){
+		//System.out.println(runQuadratic(-999, 61));
+		
+		for(int a=-1000;a<1000;a++){
+			for(int b=-1000;b<1000;b++){
 				if(isPrime(b)){
 					int numberOfPrimes=runQuadratic(a, b);
 					if(numberOfPrimes>maxNumOfPrimes){
 						maxNumOfPrimes=numberOfPrimes;
 						coefProd=a*b;
+						theA=a;
+						theB=b;
 					}
 				}
 			}
 		}
-		System.out.println("product of coefficients with most primes is "+coefProd+" with "+maxNumOfPrimes+" primes in its sequence");
+		System.out.println("product of coefficients with most primes is "+coefProd+" with "+maxNumOfPrimes+" primes in its sequence and coefficient of a " +
+				""+theA+" and b "+theB);
 		
 	}
-	public static int runQuadratic(int a,int b){
-		boolean isNotPrime=true;
+	public static int runQuadratic(long a,long b){
 		int n=0;
-		int result=0;
-		int counter=0;
-		while(isNotPrime){
-			result = n*n+a*n+b;
-			isNotPrime=isPrime(result);
-			counter++;
+		while(isPrime(Math.abs(n*n+a*n+b))){
 			n++;
 		}
-		System.out.println(counter-1+" "+a+" "+b);
-		return counter-1;
+		return n-1;
 	}
 	
-	public static boolean isPrime(int n){
-		int maxDivisor = n/2+1;
-		for(int i=2;i<maxDivisor;i++){
+	public static boolean isPrime(long n){
+		long maxDivisor = (long) (Math.sqrt(n)-1);
+		for(long i=2;i<maxDivisor;i++){
 			if(n%i==0){
 				return false;
 			}
